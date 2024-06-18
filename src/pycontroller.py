2cd3e235ad1f20c3b6e42a8cac9ce0b773987c84
Gabertho <gabrielbertho2014@hotmail.com>
2024-06-18 13:48:01 +0200

    work on python control

--- src/pycontroller.py
+++ src/pycontroller.py
@@ -84,10 +84,10 @@ def battery_callback(data):
         return
     current_battery_level = data.percentage
     ##thrust = get_thrust_based_on_battery(int(current_battery_level*100), battery_data)
-    if current_battery_level > 0.8:
-        thrust = 39.0 + (41.5-39.0)*(current_battery_level-0.8)/0.2
+    if current_battery_level > 80.0:
+        thrust = 39.0 + (41.5-39.0)*(current_battery_level-80.0)/20.0
     else:
-        thrust = 45.0 - (45.0-39.0)*current_battery_level/0.8
+        thrust = 45.0 - (45.0-39.0)*current_battery_level/80.0
     rospy.loginfo(f"Received battery level: {current_battery_level} - {thrust}")
     controller.set_hover_thrust(thrust)
     return
