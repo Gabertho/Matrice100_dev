693ab6448d9ca94dba81afc78f73becda40b0067
Gabertho <gabrielbertho2014@hotmail.com>
2024-06-19 10:26:49 +0200

    work on python control

--- src/trajectory.py
+++ src/trajectory.py
@@ -47,7 +47,7 @@ def joy_callback(data):
         # trajectory.reset(current_pose.pose.position.x, current_pose.pose.position.z, current_pose.pose.position.z)
         trajectory.have_initial_position
 
-    trajectory.move_target(-data.axes[0]/50.0, data.axes[1]/50.0, data.axes[2]/50.0)
+    trajectory.move_target(-data.axes[0], data.axes[1], data.axes[2])
         
         
 def pose_callback(data):
