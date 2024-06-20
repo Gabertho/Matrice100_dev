4f58c7e6502f810584af8e64ab44d048b379f74a
Tommy Persson <tommy.persson@liu.se>
2024-06-20 12:12:36 +0200

    Trajectory full.

--- src/goto_trajectory.py
+++ src/goto_trajectory.py
@@ -142,8 +142,8 @@ class GotoTrajectory:
         pathmsg = Path()
 
         pathmsg.header.frame_id = "world"
-        pathmsg.header.stamp.sec = 0
-        pathmsg.header.stamp.nsec = 1000000000.0*dt
+        pathmsg.header.stamp.secs = 0
+        pathmsg.header.stamp.nsecs = 1000000000.0*dt
 
         while phase != "hover":
             # If we are accelerating, then increase speed (v = v0 + a.t)
