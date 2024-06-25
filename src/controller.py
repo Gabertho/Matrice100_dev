40acced4265251a42fb92cf92bc6433c0375fec3
Gabertho <gabrielbertho2014@hotmail.com>
2024-06-25 11:12:02 +0200

    work on python control

--- src/controller.py
+++ src/controller.py
@@ -327,7 +327,7 @@ class Controller:
         ithrust = 0.0019
         dthrust = 0.0
         #pthrust = 1.5
-        ithrust = 0.0
+        #ithrust = 0.0
         #dthrust = 0.0
         velthrust = 6.0
 
