3014b4f47a23938808b4e1bf68f478f2eb434df1
Tommy Persson <tommy.persson@liu.se>
2024-06-26 18:35:07 +0200

    Work on spline trajectory.

--- src/spline_trajectory.py
+++ src/spline_trajectory.py
@@ -26,11 +26,7 @@ class SplineTrajectory:
         self.targets = [np.array([x, y, z])]
         self.target_speed = speed
         self.acc = 1.0
-        self.phase = "acc"
-        self.have_initial_position = False
         self.have_initial_position_from_pose = False
-        self.speed = 0.0
-        self.travel_length = 0.0
         self.joy_x = 0.0
         self.joy_y = 0.0
         self.joy_z = 0.0
@@ -68,7 +64,7 @@ class SplineTrajectory:
     
     def update_spline(self):
         try:
-            if not self.have_initial_position_from_pose and not self.have_initial_position:
+            if not self.have_initial_position_from_pose:
                 return
             (length, lengths) = self.get_target_path_length()
             tot_length = length
@@ -117,7 +113,7 @@ class SplineTrajectory:
         
     def get_path_points(self):
         res = []
-        if not self.have_initial_position_from_pose and not self.have_initial_position:
+        if not self.have_initial_position_from_pose:
             return res
 
         times = np.arange(0.0, self.tot_time, 0.1)
@@ -179,25 +175,6 @@ class SplineTrajectory:
     #
     def reset(self):
         return
-        if not self.have_initial_position:
-            return
-        # Calcule distance from actual trajectory position to target.
-        dx = self.target_x - self.x
-        dy = self.target_y - self.y
-        dz = self.target_z - self.z
-        self.target_yaw = math.atan2(dy, dx)
-        len = math.sqrt(dx*dx+dy*dy+dz*dz)
-        if len < 0.001:
-            return
-        self.travel_length = len
-        #Calculate distance in x,y,z directions
-        self.frac_x = dx/len
-        self.frac_y = dy/len
-        self.frac_z = dz/len
-        # Reset acc_len and speed and starts acc phase.
-        self.phase = "acc"
-        self.acc_len = 0.0
-        self.speed = 0.0
 
     def get_point_stamped(self):
         msg = PointStamped()
@@ -235,18 +212,8 @@ class SplineTrajectory:
         return msg
 
     def set_initial_position(self, x, y, z):
+        # This is target/pose in got.Not neededhere since pressing button sets the start point to last target in spline
         return
-        if self.enabled_flag:
-            return
-        print("set_initial_position:", x, y, z)
-        self.x = x
-        self.y = y
-        self.z = z
-        self.targets = [np.array([x, y, z])]
-        self.target_index = 0
-        if not self.have_initial_position:
-            self.reset()
-            self.have_initial_position = True
 
     # Get the full path, i.e, sequence of points in time that lead the initial position to the target position.
     def get_path(self, dt):
@@ -305,120 +272,6 @@ class SplineTrajectory:
 
         # print("RETURN pathmsg")
         return pathmsg
-        
-        x = self.x
-        y = self.y
-        z = self.z
-        phase = "acc"
-        speed = 0.0
-        acc_len = 0.0
-
-        print("X Y Z", x, y, z)
-        print("TARGET X Y Z", self.target_x, self.target_y, self.target_z)
-
-        self.reset()
-
-        dx = self.target_x - x
-        dy = self.target_y - y
-        dz = self.target_z - z
-        dist_to_target = math.sqrt(dx*dx+dy*dy+dz*dz)
-
-        if dist_to_target < 0.05:
-            msg = PoseStamped()
-            msg.header.frame_id = "world"
-            msg.pose.position.x = x
-            msg.pose.position.y = y
-            msg.pose.position.z = z
-            msg.pose.orientation.x = 0.0
-            msg.pose.orientation.y = 0.0
-            msg.pose.orientation.z = 0.0
-            msg.pose.orientation.w = 1.0
-            msg2 = PoseStamped()
-            msg2.header.frame_id = "world"
-            msg2.pose.position.x = x
-            msg2.pose.position.y = y
-            msg2.pose.position.z = z
-            msg2.pose.orientation.x = 0.0
-            msg2.pose.orientation.y = 0.0
-            msg2.pose.orientation.z = 0.0
-            msg2.pose.orientation.w = 1.0
-            pathmsg.poses.append(msg)
-            pathmsg.poses.append(msg2)
-            print("RETURN hover pathmsg")
-            return pathmsg
-
-
-        # While we are not hovering, i.e, we didn't reach the target position:
-        while phase != "hover":
-            #Calculate distance from actual trajectory position to target.
-            dx = self.target_x - x
-            dy = self.target_y - y
-            dz = self.target_z - z
-            dist_to_target = math.sqrt(dx*dx+dy*dy+dz*dz)
-            # If we are accelerating:
-            if phase == "acc":
-                # Increase the speed (v = v0+a.t)
-                speed += self.acc*dt
-                # If speed becomes higher than target desired speed, than cruise (moves with constant target speed).
-                if speed > self.target_speed:
-                    speed = self.target_speed
-                    phase = "cruise"
-                    print("ACCLEN:", acc_len)
-                else:
-                    # If we the total distance traveled in acceleration phase becomes equal/higher than half of total distance, than go brake (desacelerate)
-                    if acc_len >= self.travel_length/2.0:
-                        phase = "brake"
-                
-            # If we are braking, then decrease speed (v = v0 - a.t)
-            if phase == "brake":
-                speed -= self.acc*dt
-                # If our speed becomes negative, than hover (stops moving -> reached target).
-#                if speed < 0.0 or (dist_to_target < 0.05):
-                if speed < 0.0:
-                    speed = 0.0
-                    phase = "hover"
-                    print("HOVER:")
-    
-            if phase == "cruise":
-                # If we are cruisng and the distance to target becomes smaller than distance traveled in acceleration, then go brake.
-                if dist_to_target <= acc_len:
-                    phase = "brake"
-
-            # If we are hovering, than our position is equal target position (because we reached it)
-            if phase == "hover":
-                pass
-                #x = self.target_x
-                #y = self.target_y
-                #z = self.target_z
-
-            # Calculate new trajectory point (distance = s0+v.t)
-            mylen = speed*dt
-            ## print("FULL TRAJECTORY SPEED:", speed, self.frac_x, self.frac_y, self.frac_z, mylen, acc_len, dist_to_target)
-            # Updates trajectory position.
-            x += self.frac_x*mylen
-            y += self.frac_y*mylen
-            z += self.frac_z*mylen
-            ## print("POSITION:", phase, x, y, z)
-            if phase == "acc":
-                acc_len += mylen
-
-            msg = PoseStamped()
-            msg.header.frame_id = "world"
-            msg.pose.position.x = x
-            msg.pose.position.y = y
-            msg.pose.position.z = z
-            msg.pose.orientation.x = 0.0
-            msg.pose.orientation.y = 0.0
-            msg.pose.orientation.z = 0.0
-            msg.pose.orientation.w = 1.0
-
-            if phase == "hover":
-                pathmsg.poses[-1] = msg
-            else:
-                pathmsg.poses.append(msg)
-
-        print("RETURN pathmsg")
-        return pathmsg
 
     def move_tick(self):
         # Moving target position with joystick.
@@ -430,64 +283,7 @@ class SplineTrajectory:
         
     # Tick: Updates trajectory position in each iteration.
     def tick(self, dt):
+        # This mode does not work for spline
         return
-        if not self.enabled_flag:
-            return
-
-        print("goto_trajectory tick:", dt, self.phase, self.enabled_flag)
-
-        
-        # Calculating distance from trajectory position to target.
-        dx = self.target_x - self.x
-        dy = self.target_y - self.y
-        dz = self.target_z - self.z
-        dist_to_target = math.sqrt(dx*dx+dy*dy+dz*dz)
-        print("dist_to_target:", dist_to_target)
-
-        # If we are accelerating, then increase speed (v = v0 + a.t)
-        if self.phase == "acc":
-            self.speed += self.acc*dt
-            # If speed becomes higher than target desired speed, than cruise (moves with constant target speed).
-            if self.speed > self.target_speed:
-                self.speed = self.target_speed
-                self.phase = "cruise"
-                print("ACCLEN:", self.acc_len)
-            else:
-                # If we the total distance traveled in acceleration phase becomes equal/higher than half of total distance, than go brake (desacelerate)
-                if self.acc_len >= self.travel_length/2.0:
-                    self.phase = "brake"
-                
-        # If we are braking, then decrease speed (v = v0 - a.t)
-        if self.phase == "brake":
-            self.speed -= self.acc*dt
-            # If our speed becomes negative, than hover (stops moving -> reached target).
-            if self.speed < 0.0 or (dist_to_target < 0.05):
-                self.speed = 0.0
-                self.phase = "hover"
-                print("HOVER:")
-    
-        if self.phase == "cruise":
-            # If we are cruisng and the distance to target becomes smaller than distance traveled in acceleration, then go brake.
-            if dist_to_target < self.acc_len:
-                self.phase = "brake"
-
-        # If we are hovering, than our position is equal target position (because we reached it)
-        if self.phase == "hover":
-            pass
-            #self.x = self.target_x
-            #self.y = self.target_y
-            #self.z = self.target_z
-
-        # Calculate new trajectory point (distance = s0+v.t)
-        len = self.speed*dt
-        print("SPEED:", self.speed, self.frac_x, self.frac_y, self.frac_z, len)
-        # Updates trajectory position.
-        self.x += self.frac_x*len
-        self.y += self.frac_y*len
-        self.z += self.frac_z*len
-        print("POSITION:", self.x, self.y, self.z)
-        if self.phase == "acc":
-            self.acc_len += len
-        
 
             
