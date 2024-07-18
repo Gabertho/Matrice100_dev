027dae15f51fa477d409a1fed91c6e00056ab0a5
Gabertho <gabrielbertho2014@hotmail.com>
2024-07-18 15:53:33 +0200

    Woking on LQR Controller

--- src/controller.py
+++ src/controller.py
@@ -370,8 +370,18 @@ class Controller:
                 state = np.array([rherror[0], rherrorvel[0], rherror[1], rherrorvel[1]])
                 control_input = self.lqr_control(state, self.K)
     
-                u[0] = control_input[0] # roll
-                u[1] = control_input[1] # pitch
+                u[0] = -math.radians(control_input[0]) # roll
+                u[1] = -math.radians(control_input[1]) # pitch
+
+                max = math.radians(20.0)
+                if u[0] > max:
+                    u[0] = max
+                if u[0] < -max:
+                    u[0] = -max
+                if u[1] > max:
+                    u[1] = max
+                if u[1] < -max:
+                    u[1] = -max
 
 
             if self.mode == "simple_pid":
