3fd8ec683b7b62ea5504c50c5bd346a5b818a29d
Gabertho <gabrielbertho2014@hotmail.com>
2024-07-19 10:31:22 +0200

    Working on MRAC

--- src/controller.py
+++ src/controller.py
@@ -464,7 +464,7 @@ class Controller:
                 control_action = -self.Kbar @ state
 
                 # Definir roll e pitch a partir da ação de controle
-                u[0] = math.radians(control_action[0])  # Roll
+                u[0] = -math.radians(control_action[0])  # Roll
                 u[1] = math.radians(control_action[1])  # Pitch
 
                 max = math.radians(20.0)
