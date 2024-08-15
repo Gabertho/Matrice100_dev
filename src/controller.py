8e3f421569c0001b17c313c67f018f568efa45d4
Gabertho <gabrielbertho2014@hotmail.com>
2024-08-15 13:59:48 +0200

    Testing increasing LR in MRAC

--- src/controller.py
+++ src/controller.py
@@ -197,7 +197,7 @@ class Controller:
         self.P_lyap_thrust = sp.solve_continuous_lyapunov(self.Am_thrust, self.Q_lyap_thrust)
         # Adaptive Parameters
         self.Wmrac_thrust = np.zeros((7,3))  # Adjust dimensions based on Phi(x)
-        self.Gammamrac_thrust = 0.01 * np.eye(7)  # Learning rate matrix, set to 0.01  # Learning rate matrix
+        self.Gammamrac_thrust = 0.1 * np.eye(7)  # Learning rate matrix, set to 0.1  # Learning rate matrix
 
         self.W_thrust = np.zeros((10,3))  # Adjust dimensions based on Phi(x)
         self.Gamma_thrust = 0.01 * np.eye(10)  # Learning rate matrix, set to 0.01  # Learning rate matrix
