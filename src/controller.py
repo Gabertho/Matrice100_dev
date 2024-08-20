cac0277aee2a3659886e4a486f7ab3031337d31b
Gabertho <gabrielbertho2014@hotmail.com>
2024-08-20 17:27:46 +0200

    Test extreme wind - DMRAC

--- src/controller.py
+++ src/controller.py
@@ -220,7 +220,7 @@ class Controller:
         #self.Gamma_thrust = 0.001 * np.eye(10)  # Learning rate matrix, set to 0.01  # Learning rate matrix
 
         self.W_thrust = np.zeros((16,3))  # Adjust dimensions based on Phi(x)
-        self.Gamma_thrust = 0.001 * np.eye(16)  # Learning rate matrix, set to 0.01  # Learning rate matrix
+        self.Gamma_thrust = 0.01 * np.eye(16)  # Learning rate matrix, set to 0.01  # Learning rate matrix
 
         #self.W_thrust = np.zeros((64,3))  # Adjust dimensions based on Phi(x)
         #self.Gamma_thrust = 0.001 * np.eye(64)  # Learning rate matrix, set to 0.01  # Learning rate matrix
