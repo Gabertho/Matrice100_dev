c79083aeccb2a70cb66ba25dd2478b392153da7b
Gabertho <gabrielbertho2014@hotmail.com>
2024-08-20 18:13:12 +0200

    Test extreme wind - DMRAC

--- src/controller.py
+++ src/controller.py
@@ -54,12 +54,12 @@ class Net(nn.Module):
         self.HL1 = nn.Linear(6, 32)
         self.HL2 = nn.Linear(32, 16)  # Ajuste o tamanho de saída para 6
         self.OL = nn.Linear(16, 3)
-        self.optimizer = optim.Adam(self.parameters(), lr=0.001)
+        self.optimizer = optim.Adam(self.parameters(), lr=0.0005)
         self.loss_fn = nn.MSELoss()
 
     def forward(self, x):
-        OL_1 = torch.relu(self.HL1(x))
-        OL_2 = torch.relu(self.HL2(OL_1))
+        OL_1 = torch.tanh(self.HL1(x))
+        OL_2 = torch.tanh(self.HL2(OL_1))
         OL_3 = self.OL(OL_2)
         return OL_2, OL_3
 
@@ -220,7 +220,8 @@ class Controller:
         #self.Gamma_thrust = 0.001 * np.eye(10)  # Learning rate matrix, set to 0.01  # Learning rate matrix
 
         self.W_thrust = np.zeros((16,3))  # Adjust dimensions based on Phi(x)
-        self.Gamma_thrust = 0.001 * np.eye(16)  # Learning rate matrix, set to 0.01  # Learning rate matrix
+        #self.Gamma_thrust = 0.001 * np.eye(16)  # Learning rate matrix, set to 0.01  # Learning rate matrix
+        self.Gamma_thrust = 0.01 * np.eye(16)
 
         #self.W_thrust = np.zeros((64,3))  # Adjust dimensions based on Phi(x)
         #self.Gamma_thrust = 0.001 * np.eye(64)  # Learning rate matrix, set to 0.01  # Learning rate matrix
@@ -230,7 +231,7 @@ class Controller:
         self.replay_buffer = []
         self.buffer_size = 1000
         self.batch_size = 64
-        self.zeta_tol = 0.0003
+        self.zeta_tol = 0.0004
 
         #Counter for triggering DNN training
         self.new_samples_collected = 0
