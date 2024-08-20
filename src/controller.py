683fcaef5f767c1f1c38a4b7a50ea6a4b30785d2
Gabertho <gabrielbertho2014@hotmail.com>
2024-08-20 15:36:12 +0200

    Test extreme wind - PID

--- src/controller.py
+++ src/controller.py
@@ -92,7 +92,7 @@ class Controller:
         self.old_err_yaw = 0.0
         self.old_err_z = 0.0
         self.yaw_control_flag = False
-        self.mode = "LQR_thrust"
+        self.mode = "simple_pid"
         self.trajectory_flag = False
         self.full_trajectory_x = None
         self.full_trajectory_y = None
